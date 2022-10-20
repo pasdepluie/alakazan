a2372d4cca254a2bf738c0cf645a2bb5d741e92e
Manoel <njmlima@outlook.com>
2022-10-20 20:49:05 -0300

    commit para outro ramo

--- tempimento.py
+++ tempimento.py
@@ -5,5 +5,6 @@ from bokeh.plotting.figure import figure
 seno = numpy.sin(numpy.pi)
 
 p = figure(width=1280, height=720)
-p.line(x=, y=numpy.linspace(-numpy.pi, numpy.pi, 255))
+p.line(x=[-64,64], y=numpy.linspace(-numpy.pi, numpy.pi, 255))
+
 show(p)
