a9f6b37a6aeb3921116dc3c5e04a9b0e0fcdde71
Manoel <njmlima@outlook.com>
2022-10-20 20:31:35 -0300

    e se eu fizesse um banco nosql das compras

--- tempimento.py
+++ tempimento.py
@@ -1,1 +1,9 @@
 import numpy
+import bokeh
+from bokeh.plotting.figure import figure
+
+seno = numpy.sin(numpy.pi)
+
+p = figure(width=1280, height=720)
+p.line(x=, y=numpy.linspace(-numpy.pi, numpy.pi, 255))
+show(p)
